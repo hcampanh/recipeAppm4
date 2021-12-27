b79c467f1a65f8eac16b52f7dc9820cba759ad96
henrique campanha <henriqueoc@msn.com>
2021-12-27 16:22:39 +0000

    Initial Commit

---
+++ RecipeListApp/RecipeListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListAppApp.swift
+//  RecipeListApp
+//
+//  Created by henrique campanha on 27/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
